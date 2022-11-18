bb48ab9a75a043177c9ff81f8dbb5452ed50d9fb
Юлия Санникова <sannikovayi@gmail.com>
2022-11-18 20:19:42 +0300

    Add lines 141+

--- ml_web_app.py
+++ ml_web_app.py
@@ -117,3 +117,50 @@ with row1_2:
 
 # LAYING OUT THE MIDDLE SECTION OF THE APP WITH THE MAPS
 row2_1, row2_2, row2_3, row2_4 = st.columns((2, 1, 1, 1))
+
+# SETTING THE ZOOM LOCATIONS FOR THE AIRPORTS
+la_guardia = [40.7900, -73.8700]
+jfk = [40.6650, -73.7821]
+newark = [40.7090, -74.1805]
+zoom_level = 12
+midpoint = mpoint(data["lat"], data["lon"])
+
+with row2_1:
+    st.write(
+        f"""**All New York City from {hour_selected}:00 and {(hour_selected + 1) % 24}:00**"""
+    )
+    map(filterdata(data, hour_selected), midpoint[0], midpoint[1], 11)
+
+with row2_2:
+    st.write("**La Guardia Airport**")
+    map(filterdata(data, hour_selected), la_guardia[0], la_guardia[1], zoom_level)
+
+with row2_3:
+    st.write("**JFK Airport**")
+    map(filterdata(data, hour_selected), jfk[0], jfk[1], zoom_level)
+
+with row2_4:
+    st.write("**Newark Airport**")
+    map(filterdata(data, hour_selected), newark[0], newark[1], zoom_level)
+
+# CALCULATING DATA FOR THE HISTOGRAM
+chart_data = histdata(data, hour_selected)
+
+# LAYING OUT THE HISTOGRAM SECTION
+st.write(
+    f"""**Breakdown of rides per minute between {hour_selected}:00 and {(hour_selected + 1) % 24}:00**"""
+)
+
+st.altair_chart(
+    alt.Chart(chart_data)
+    .mark_area(
+        interpolate="step-after",
+    )
+    .encode(
+        x=alt.X("minute:Q", scale=alt.Scale(nice=False)),
+        y=alt.Y("pickups:Q"),
+        tooltip=["minute", "pickups"],
+    )
+    .configure_mark(opacity=0.2, color="red"),
+    use_container_width=True,
+)
