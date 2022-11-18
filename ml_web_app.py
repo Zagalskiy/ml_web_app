7fe309b6fd639f7cb820e06165d84107dac5fd4e
Vladimir Mayngert <gigabyte727@gmail.com>
2022-11-18 20:03:25 +0300

    Add def map lines 29 - 53

--- ml_web_app.py
+++ ml_web_app.py
@@ -26,4 +26,29 @@ def load_data():
     )
 
     return data
+# FUNCTION FOR AIRPORT MAPS
+def map(data, lat, lon, zoom):
+    st.write(
+        pdk.Deck(
+            map_style="mapbox://styles/mapbox/light-v9",
+            initial_view_state={
+                "latitude": lat,
+                "longitude": lon,
+                "zoom": zoom,
+                "pitch": 50,
+            },
+            layers=[
+                pdk.Layer(
+                    "HexagonLayer",
+                    data=data,
+                    get_position=["lon", "lat"],
+                    radius=100,
+                    elevation_scale=4,
+                    elevation_range=[0, 1000],
+                    pickable=True,
+                    extruded=True,
+                ),
+            ],
+        )
+    )
 
