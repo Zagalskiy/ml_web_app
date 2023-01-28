634a3ada19a22a77d45abb10717d06b6dc82517e
gigabyte727 <gigabyte727@gmail.com>
2023-01-28 12:17:00 +0300

    editing launch.sh

--- launch.sh
+++ launch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo 'Запускаем сервер streamlit'
 cd ml_web_app/
-source /home/gigabyte727/python_venv/streamlit/bin/activate (При корректном запуске перед пользователем появится надпись (streamlit) )
+source ../python_venv/streamlit/bin/activate
 streamlit run ml_web_app.py
