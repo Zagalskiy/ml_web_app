728581cf2c51a0ae69f7967026ff29048c89527b
gigabyte727 <118012782+gigabyte727@users.noreply.github.com>
2023-03-23 20:47:04 +0300

    Update test_web_app.py

--- test_web_app.py
+++ test_web_app.py
@@ -1,4 +1,6 @@
 import requests
+
+
 def test_url(): # создаём функцию проверки работоспособности Web-приложения
     r = requests.head('https://nyc-uber.streamlit.app')
     assert r.status_code == 303 # проверяем статус код перенаправления
