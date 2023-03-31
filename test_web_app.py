fb6f3347b92c331d46b971b11c1fcd2edf2972c8
ElenaMureiko <lena.mureiko@gmail.com>
2023-03-31 21:04:13 +0300

    Testl

--- test_web_app.py
+++ test_web_app.py
@@ -1,6 +1,5 @@
 import requests
 
-
 def test_url():    # создаём функцию проверки работоспособности Web-приложения
     r = requests.head('https://nyc-uber.streamlit.app')
     assert r.status_code == 303    # проверяем статус код перенаправления
